ab06703bc46c80e9e7a661319c0a06ee46708d2e
bppham <phubao180803@gmail.com>
2025-04-06 16:37:18 +0700

    feat: home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,54 @@
-"use client"
-import React from 'react'
-import './home.css'
-import Chart from '../components/Chart/Chart'
-import FeaturedInfo from '../components/FeaturedInfo/FeaturedInfo'
-import WidgetSmall from '../components/WidgetSmall/WidgetSmall'
-import WidgetLarge from '../components/WidgetLarge/WidgetLarge'
-import{userData} from "../components/dummy-data"
+"use client";
+import React, { useEffect, useState } from "react";
+import "./home.css";
+import Chart from "../components/Chart/Chart";
+import FeaturedInfo from "../components/FeaturedInfo/FeaturedInfo";
+import WidgetSmall from "../components/WidgetSmall/WidgetSmall";
+import WidgetLarge from "../components/WidgetLarge/WidgetLarge";
+import { getMonthlyOrders } from "../api/home";
 const home = () => {
-  return (
-    <div className='home'>
-        <FeaturedInfo/>
-        <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-        <div className="home-widget">
-          <WidgetSmall/>
-          <WidgetLarge/>
-        </div>
-    </div>
-  ) 
-}
+  const [orderData, setOrderData] = useState([]);
 
-export default home
+  useEffect(() => {
+    fetchMonlyOrders();
+  }, []);
+
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const fetchMonlyOrders = async () => {
+    try {
+      const response = await getMonthlyOrders();
+      if (response.code === 200) {
+        const englishMonthData = response.data.map((item, index) => ({
+          name: monthNames[index], // thay thế tên tháng
+          total: item.total,
+        }));
+        setOrderData(englishMonthData);
+      }
+    } catch (error) {
+      console.log("Error get order stats");
+    }
+  };
+
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart data={orderData} title="Order Analytics" grid dataKey="total" />
+    </div>
+  );
+};
+
+export default home;
