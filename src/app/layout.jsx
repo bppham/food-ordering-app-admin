69024e6f13bb6da2ae732f41e39544edaaf45f01
bppham <phubao180803@gmail.com>
2025-04-15 20:47:48 +0700

    add interceptor for admin page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,24 +1,31 @@
-"use client";
+'use client';
+
 import Sidebar from "../components/Sidebar/Sidebar";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
 import { usePathname } from "next/navigation"; 
-export default function RootLayout({ children }) {
+import RequireAuth from "../components/RequireAuth";
 
+export default function RootLayout({ children }) {
   const pathname = usePathname();  
   const isAuthPage = pathname.startsWith("/auth");
+
+  const content = (
+    <>
+      {!isAuthPage && <Navbar />}
+      <div className="admin-container">
+        {!isAuthPage && <Sidebar />}
+        <div className="admin-container-content">
+          {children}
+        </div>
+      </div>
+    </>
+  );
 
   return (
     <html lang='en'>        
       <body>
-        {!isAuthPage && <Navbar />}
-        <div className="admin-container">
-          {!isAuthPage && <Sidebar />}
-          <div className="admin-container-content">
-            { children }
-          </div>
-        </div>
-       
+        {isAuthPage ? content : <RequireAuth>{content}</RequireAuth>}
       </body>
     </html>
   );
